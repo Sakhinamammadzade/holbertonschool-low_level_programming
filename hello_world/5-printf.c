299c3712684a18d8223c73b455b5a1bedddb8353
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-12 06:47:48 -0700

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main -with proper grammar, but the outcome is a piece of art,
- *
- * Return: Always 0 (Success)
+* main -with proper grammar, but the outcome is a piece of art,
+*
+* Return: Always 0 (Success)
 **/
 int main(void)
 {
