8c6e439f7a547456a779a4c8c3bea5418808064c
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-12 07:01:04 -0700

    c printf funcs

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-*main - Prints with proper grammar, but the outcome is a piece of art,.
+*main -with proper grammar, but the outcome is a piece of art,.
 *
 *Return: Always 0.
 */
