581e00e33a12c39389f4bd993373b8da2657759d
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-12 06:59:26 -0700

    c printf func

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main -with proper grammar, but the outcome is a piece of art,
+*main - Prints with proper grammar, but the outcome is a piece of art,.
 *
-* Return: Always 0 (Success)
-**/
+*Return: Always 0.
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
-}    
+return (0);
+}
