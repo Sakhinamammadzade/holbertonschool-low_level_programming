5d0f04d11828dc8c95514888f6a82502328bb82a
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-12 06:32:43 -0700

    betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - function
  *
  * Return: Always 0 (Success)
 **/
-
 int main(void)
 {
-      puts("\"Programming is like building a multilingual puzzle");
-      return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
