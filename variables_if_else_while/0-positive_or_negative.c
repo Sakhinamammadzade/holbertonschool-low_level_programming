3b650144cff703a097be0b15da0afb8d503146ef
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-15 14:49:54 -0700

    first file added

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,14 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+  printf("is positive\n");
+else if (n == 0)
+  printf("is zero");
+else
+  printf("is negative\n");
+return (0);
 }
