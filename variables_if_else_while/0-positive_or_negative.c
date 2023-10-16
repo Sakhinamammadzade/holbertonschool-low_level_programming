c4b7b6218583af8de2929a9907a49e79c765b869
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-16 04:37:29 -0700

    some checks

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,21 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-/* main function -check random number positive or negative */
-/* betty style doc for function main goes there */
+/**
+*main - function checks random rumber positive or zero
+*
+*Return: Always 0.
+*/
 int main(void)
 {
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-  printf("%d\n is positive", n);
+printf("%d is positive \n", n);
 else if (n == 0)
-printf("%d\n is zero", n);
+printf("%d  is zero \n", n);
 else
-printf("%d\n is negative", n);
+printf("%d is negative \n", n);
 return (0);
 }
