cb88d3b6853bb053908566dbc49c7743c9a5576a
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-16 03:43:15 -0700

    try try againnnnn

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf(n ,"is positive  %d\n");
+printf(n,"is positive  %d\n");
 else if (n == 0)
-printf(n,"is zero  %d\n");
+printf(n ,"is zero  %d\n");
 else
-printf(n,"is negative  %d\n");
+printf(n ,"is negative  %d\n");
 return (0);
 }
