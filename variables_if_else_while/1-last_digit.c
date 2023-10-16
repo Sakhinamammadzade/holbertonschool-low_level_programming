d14b6e565a03e7f47efc64d142515206972224c7
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-16 05:01:19 -0700

    last digit checks

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,11 +11,11 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 5)
-printf("%d and is grater than\n", n);
-else if (n == 0)
-printf("%d and is zero\n", n);
-else if (n < 6 && n != 0)
-printf("%d and is less than 6 and not 0\n", n);
+if ((n % 10) == 0)
+printf("Last digit of %d is %d and is 0\n", n, n % 10);
+else if ((n % 10) > 5)
+printf("Last digit of %d is %d grater than \n", n, n % 10);
+else
+printf("Last digit of %d is %d less than 6 and not 0\n", n, n % 10);
 return (0);
 }
