c7830cc4a5084c4fbfeca34dfc9c9d0589eadb38
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-17 09:22:07 -0700

    0-99

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -11,15 +11,21 @@ for (i = 0 ; i <= 99 ; i++)
 {
 for (j = i ; j <= 99 ; j++)
 {
-putchar(i);  
+if (i != j)
+{
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
 putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
+if (!(i == 98 && j == 99))
+{
 putchar(',');
 putchar(' ');
 }
 }
+}
+}
+putchar('\n');
 return (0);
 }
