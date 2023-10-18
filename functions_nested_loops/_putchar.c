e60bd805187675e142565f6bb02e1fa51652e7da
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-18 15:43:47 -0700

    _putchar

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -8,5 +9,5 @@
  */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-_putchar(void);
+int _putchar(char c);
 
 
 #endif
