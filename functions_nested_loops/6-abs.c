d4e88f68e39141d9688a5c5424d0b368ba8a65ac
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-19 06:30:24 -0700

    change absolute n

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _abs - function the absolute value of an integer
+*
+*@n: - checks int n
+* Return: 1 for letters. Returns 0 otherwise
+*/
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else
+{
+return (-1 * n);
+}
+}
