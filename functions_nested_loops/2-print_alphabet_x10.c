2921a51c41d6ab1cf6188487a65f13931842af77
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-19 02:52:20 -0700

    10times print alphabets

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - function print_alphabet _putchar
+*
+*Return: Always 0.
+*/
+void print_alphabet_x10(void)
+{
+int a, i;
+for (i = 0; i < 10 ; i++)
+{
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
+}
