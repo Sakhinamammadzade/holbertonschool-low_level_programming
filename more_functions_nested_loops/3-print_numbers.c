74be5c098a9e72d9ce410483e5d7b8c256ba2aa5
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-24 07:36:28 -0700

    print-numbers betty style error fixed

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -4,10 +4,10 @@
 * Return: 1 if digit and 0 if anything else
 */
 
-void print_numbers(void){
-char  c;
-for(c = 0; c <= 9; c++)
-_putchar(c + '0');	
+void print_numbers(void)
+{
+char c;
+for (c = 0; c <= 9; c++)
+_putchar(c + '0');
 _putchar('\n');
-
 }
