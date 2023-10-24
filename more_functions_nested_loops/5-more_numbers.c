c47d191a5c0eec82df7ff27582e68fd3fc30314f
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-24 08:31:35 -0700

    print ascii

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*more_numbers- function print 10times numbers
+*
+*Return: Always 0.
+*/
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j > 9)
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+}
+_putchar('\n');
+}
+}
