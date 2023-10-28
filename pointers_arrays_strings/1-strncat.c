4f4b6fe836bcb57d04929df33f178b99a1157a12
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-10-28 06:59:39 -0700

    change strnfile

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,19 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int i = 0;
-int j;
+int j = 0;
 while (dest[i] != '\0')
 i++;
-for (j = 0; j < n && src[j] != '\0'; j++, i++)
+while (src[j] != '\0')
 {
 dest[i] = src[j];
+if (src[i] != n)
+{
+break;
 }
+j++;
+i++;
 dest[i] = '\0';
+}
 return (dest);
 }
-
