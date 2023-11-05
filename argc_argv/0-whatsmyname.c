7002f037a000e24d24bc58e90379003cbec9bf54
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-11-05 06:16:48 -0800

    argc argv 0 task

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -  writes its name
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf ("%s\n", argv[0]);
+return (0);
+}
