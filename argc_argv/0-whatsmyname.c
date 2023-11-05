af5ed28716d5a01b5f1ae35d3c1a301f28b7ea98
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-11-05 06:20:14 -0800

    betty error fix

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,10 +5,9 @@
  * @argv: array of pointers to arguement strings
  * Return: 0
  **/
-int main (int argc, char *argv[])
-
+int main(int argc, char *argv[])
 {
 (void) argc;
-printf ("%s\n", argv[0]);
+printf("%s\n", argv[0]);
 return (0);
 }
