2a34ee20330c1ac31b8e5145bba80b0082a2699f
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-11-20 16:07:26 -0800

    struct first task

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - function to typedef struct dog
+ * @name: character pointer
+ * @age: float
+ * @owner: character pointer
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_h;
+
+#endif
