2674f73c6b577d4e1f623fef8682b7b057ee97d3
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2023-11-21 03:10:44 -0800

    modified task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,15 +9,16 @@
  */
 void print_dog(struct dog *d)
 {
-if (d == NULL)
-return;
-if (d->name == NULL)
-(d->name = "nil");
-else
-printf("Name : %s\n", d->name);
-printf("Age : %.6f\n", d->age);
-if (d->owner == NULL)
-(d->owner = "nil");
-else
-printf("Owner : %s\n", d->owner);
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %.6f\n", d->age);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
 }
