a92e073f651a5a37938e3e8c79b7caa2eb043830
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-01-18 07:02:45 -0800

    betty error fixed

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,23 +3,21 @@
  *linear_search- func that searches for a value in array using Linear search
  *@array: point to array
  *@size: size of array
- *value:seaching value
+ *@value:seaching value
  *Return: index of the value if found, otherwise -1
  */
 int linear_search(int *array, size_t size, int value)
 {
 size_t i;
 if (array == NULL || size == 0)
-	return -1;
-for(i = 0; i < size; i++)
+	return (-1);
+for (i = 0; i < size; i++)
 {
 printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-if(array[i] == value)
+if (array[i] == value)
 {
-printf("Value %d found at index %lu\n", value, i);
-return i;
+return (i);
 }
 }
-return -1;
+return (-1);
 }
- 
