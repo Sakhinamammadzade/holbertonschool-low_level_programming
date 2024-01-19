f35ba2c27780c8abe4bdc3f7db8c794bbe79fe79
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-01-19 13:41:25 -0800

    binary search

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+/**
+ *binary_search - func searches for a value sorted array Binary
+ *@array:point to array
+ *@size:size of array
+ *@value:is the value  to search for
+*/
+int binary_search(int *array, size_t size, int value)
+{
+size_t i, left, right;
+
+if (array == NULL || value == 0)
+	return (-1);
+
+for (left = 0, right = (size - 1); left <= right; left++)
+{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+}
+return (-1);
+}
