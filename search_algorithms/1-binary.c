11a2d451ec1e4966099b26e5e5b91c5ddbaedc0d
Sakhinamammadzade <sakhinamammadzade0406@gmail.com>
2024-01-19 13:51:28 -0800

    kernel style code

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -4,6 +4,7 @@
  *@array:point to array
  *@size:size of array
  *@value:is the value  to search for
+ *Return: index of the value in the array, or -1 if not found
 */
 int binary_search(int *array, size_t size, int value)
 {
@@ -14,18 +15,18 @@ if (array == NULL || value == 0)
 
 for (left = 0, right = (size - 1); left <= right;)
 {
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
+printf("Searching in array: ");
+for (i = left; i < right; i++)
+	printf("%d, ", array[i]);
+printf("%d\n", array[i]);
 
-		i = left + (right - left) / 2;
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			right = i - 1;
-		else
-			left = i + 1;
+i = left + (right - left) / 2;
+if (array[i] == value)
+	return (i);
+if (array[i] > value)
+	right = i - 1;
+else
+	left = i + 1;
 }
 return (-1);
 }
